1c7482c71b12d675f00f2caf093da6dafc8bd57e
gulvarol <gulvarol90@gmail.com>
2012-12-03 21:51:41 +0200

    test

--- test/unit/occasion_test.rb
+++ test/unit/occasion_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class OccasionTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Occasion.new.valid?
-  end
+	def test_should_be_valid
+		assert Occasion.new.valid?
+	end
+
+	def properlysaved
+    	occasion = Occasion.new
+  		assert occasion.save
+	end
 end
