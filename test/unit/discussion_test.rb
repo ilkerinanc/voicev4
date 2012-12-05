04a05877b2a6360e9da9280b2b93363234639f2a
Aysegul <sariaysegulsari@gmail.com.tr>
2012-12-05 07:54:32 -0800

    a unit test for discussion control is added

--- test/unit/discussion_test.rb
+++ test/unit/discussion_test.rb
@@ -4,4 +4,17 @@ class DiscussionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_should_be_valid
+    assert Discussion.new.valid?
+  end
+
+  def test_should_be_valid2
+  	discussion = Discussion.new
+  	discussion.name = "yeni"
+    discussion.description = "hshs"
+  	discussion.in_trash = false
+  	discussion.creator_id = 2
+    assert discussion.save ,"Save discussion without name"
+  end
 end
