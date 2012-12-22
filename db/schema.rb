cb7281a99cbbd73caa934ad5f08a976d514e7f94
Aysegul <sariaysegulsari@gmail.com.tr>
2012-12-21 22:50:16 -0800

    creator_id is added to discussion post model

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215175529) do
+ActiveRecord::Schema.define(:version => 20121222062043) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -20,13 +21,10 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.integer  "count"
   end
 
-  create_table "discussion_posts", :force => true do |t|
-    t.text     "content"
-    t.boolean  "in_trash"
-    t.integer  "discussion_id"
-    t.integer  "user_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+  create_table "conversations", :force => true do |t|
+    t.string   "subject",    :default => ""
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "discussionposts", :force => true do |t|
@@ -34,15 +32,13 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.string   "content"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.integer  "creator_id"
   end
 
   create_table "discussions", :force => true do |t|
     t.string   "name"
-    t.text     "description"
-    t.boolean  "in_trash"
-    t.integer  "creator_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "event_interests", :force => true do |t|
@@ -59,12 +55,8 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.string   "place"
     t.integer  "creator_id"
     t.boolean  "in_trash"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-    t.string   "eventphoto_file_name"
-    t.string   "eventphoto_content_type"
-    t.integer  "eventphoto_file_size"
-    t.datetime "eventphoto_updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.datetime "date"
   end
 
@@ -112,6 +104,41 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "newmessages", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "read_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "notifications", :force => true do |t|
+    t.string   "type"
+    t.text     "body"
+    t.string   "subject",              :default => ""
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                :default => false
+    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                              :null => false
+    t.integer  "notified_object_id"
+    t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.string   "attachment"
+  end
+
+  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
+
   create_table "occasions", :force => true do |t|
     t.string   "name"
     t.integer  "creator_id"
@@ -122,12 +149,33 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.datetime "time"
   end
 
+  create_table "posts", :force => true do |t|
+    t.integer  "post_id"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "questions", :force => true do |t|
     t.integer  "survey_id"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "receipts", :force => true do |t|
+    t.integer  "receiver_id"
+    t.string   "receiver_type"
+    t.integer  "notification_id",                                  :null => false
+    t.boolean  "is_read",                       :default => false
+    t.boolean  "trashed",                       :default => false
+    t.boolean  "deleted",                       :default => false
+    t.string   "mailbox_type",    :limit => 25
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
+  end
+
+  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "searches", :force => true do |t|
     t.string   "keywords"
@@ -178,8 +226,8 @@ ActiveRecord::Schema.define(:version => 20121215175529) do
     t.string   "name"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.datetime "start_time"
     t.datetime "finish_time"
+    t.datetime "start_time"
   end
 
   create_table "taggings", :force => true do |t|
